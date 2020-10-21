597b1f8f1a9c8af3f2c37220e071fea3f11db895
Raimundas Juska <raimundas.juska@gmail.com>
2020-10-21 20:38:02 +0300

    selenium tests developed

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -1,25 +1,40 @@
-import random
-
 from selenium.webdriver.common.by import By
-
 from selenium_ui.base_page import BasePage
 from selenium_ui.conftest import print_timing
 from util.conf import JIRA_SETTINGS
 
-
-def app_specific_action(webdriver, datasets):
+def app_specific_action_sr(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        app_specific_issue = random.choice(datasets['custom_issues'])
-        issue_key = app_specific_issue[0]
+
+    report_url = "/plugins/servlet/simple-reports-app/reports"
+    iframe_id = "simple-reports-iframe"
 
     @print_timing("selenium_app_custom_action")
     def measure():
 
-        @print_timing("selenium_app_custom_action:view_issue")
+        @print_timing("selenium_app_custom_action:view_report")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}{report_url}")
+            page.wait_until_available_to_switch((By.ID, iframe_id)) # switch to app iframe
+            page.wait_until_visible((By.CLASS_NAME , "centered")) # loading started and spinner is visible
+            page.wait_until_invisible((By.CLASS_NAME , "centered")) # loading finished and spinner is not visible
+        sub_measure()
+    measure()
+
+def app_specific_action_dashboard(webdriver, datasets):
+    page = BasePage(webdriver)
+
+    report_url = "/secure/Dashboard.jspa?selectPageId=10100"
+    iframe_id = "dashboard-item-iframe"
+
+    @print_timing("selenium_app_custom_action")
+    def measure():
+
+        @print_timing("selenium_app_custom_action:view_report")
+        def sub_measure():
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}{report_url}")
+            page.wait_until_available_to_switch((By.ID, iframe_id)) # switch to app iframe
+            page.wait_until_visible((By.CLASS_NAME , "centered")) # loading started and spinner is visible
+            page.wait_until_invisible((By.CLASS_NAME , "centered")) # loading finished and spinner is not visible
         sub_measure()
     measure()
